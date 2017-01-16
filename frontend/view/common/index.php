263c6b16f65ac6803dbfc364b83c024053f36f1d
thaogiang.90@gmail.com <thaogiang.90@gmail.com>
2017-01-16 08:23:12 +0700

    a

---
+++ frontend/view/common/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dien may xanh</title>
+    <link href="../../../webroot/css/bootstrap.min.css" media="all" rel="stylesheet">
+    <link href="../../../webroot/css/style.css" media="all" rel="stylesheet">
+    <script src="../../../webroot/js/jquery-1.10.2.min.js"></script>
+    <script src="../../../webroot/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div id="flags" class="container-fluid">
+<div class="">
+    <div class="col-md-12">
+        <ul>
+            <li id="vn">Viet Nam</li>
+            <li id="el">English</li>
+        </ul>
+    </div>
+</div>
+</div>
+<!--End flag-->
+<div id="header">
+    <div class="container">
+        <div id="logo" class="row">
+            <div class="col-md-6 logo">
+                <a href="#"><img src="../../../webroot/img/logo.jpg"></a>
+            </div>
+            <div id="search" class="col-md-6">
+                <div id="custom-search-input" class="col-md-10">
+                    <div class="input-group">
+                        <input type="text" class="form-control input-sm" placeholder="Tìm kiếm" />
+                        <span class="input-group-btn">
+                        <button class="btn btn-info btn-lg" type="button">
+                            <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                    </span>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
